a967604461201b1da4e05f3519b2f3a4aa037461
Yury Sergeyev <60914627+Yorksergeyev@users.noreply.github.com>
2020-03-18 21:38:24 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,49 +1,80 @@
+function pr(input) {
+        input.value = input.value.replace(/[^\d,]/g, '');
+    };
 
-    //выполняется весь код
     const root = document.getElementById("root");
     const arr_symbols = ['j', 'k', 'f', 'd', " "];
     const arr_colors = ['success', 'warning', 'error', 'yellow', 'violet'];
+    let miss = 0;
+   
 
-    const input_count = 20;
-
-    function render_state(arr, count){
-        let rand_arr = [];
-        for (let i = 0; i < count; i++) {
-            let rand_numb = Math.floor(Math.random() * arr.length)
-            let newButton = document.createElement("button");
-            newButton.innerHTML = arr[rand_numb];
-            newButton.classList.add(arr_colors[rand_numb]);
-            rand_arr.push(newButton);
+    function init(){
+                sec = 0;
+                setInterval(tick, 1000);
+            }
+            
+    function tick(){
+        sec++;
+        document.getElementById("times").
+        childNodes[0].nodeValue = sec;       
         }
-        return rand_arr; 
+
+    function play(){
+        if (input.value == ""){
+            input.placeholder = "Не заполнено!"
+            input.style.border = "3px solid red";
+        }
+        else{
+            event.preventDefault();
+            const input_count = input.value;
+            root.classList.add("ten");
+            document.getElementById("del").remove();
+            time.style.display = 'flex';
+            init();
+            game(input_count);
+        }
     }
-    let final_arr = render_state(arr_symbols , input_count);
 
-    // for (let i = 0; i < 10; i++) {
-    //     root.insertAdjacentElement('beforeend', final_arr[i]);
-    // }
+    function game(input_count){
 
-    final_arr.forEach(element => {
-        root.insertAdjacentElement('beforeend', element);
-    });
-
-
-// получаем массив кнопок из DOM
-// получить их содержимое через контекст
-//если нажата кнопка key, которая совпадает с context 
-// элемент удаляется
-
-// когда элементы закончатся, вы победили
-// вы прошли уровень за столько ко секунд
-
-// let buttons = document.querySelectorAll("button");
-
-window.onkeydown = function(event){
-   let buttons = document.querySelectorAll("button");
-    if (event.key == buttons[0].textContent){
-        buttons[0].remove();        
+        function render_state(arr, count){
+            let rand_arr = [];
+            for (let i = 0; i < count; i++) {
+                let rand_numb = Math.floor(Math.random() * arr.length)
+                let newButton = document.createElement("button");
+                newButton.innerHTML = arr[rand_numb];
+                newButton.classList.add(arr_colors[rand_numb]);
+                rand_arr.push(newButton);
+            }
+            return rand_arr; 
+        }
+        let final_arr = render_state(arr_symbols , input_count);
+    
+        final_arr.forEach(element => {
+            root.insertAdjacentElement('beforeend', element);
+        });
+    
+    
+    
+    window.onkeydown = function(event){
+       let buttons = document.querySelectorAll("button");
+        if (event.key == buttons[0].textContent){
+            if(buttons.length == 1){ 
+                document.getElementById("time").remove();  
+                alert('Вы затратили '+sec+' sec и '+miss+' ошибки');
+                location.reload(true)
+            }
+                buttons[0].remove();     
+        }
+        else {
+            miss++;
+        }
     }
-}
+    }
+
+
+
+
 
 
 
